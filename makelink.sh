87ba1fed2e7acb0101b38e35157916b1be53709a
Plato Wu <gtalk000@gmail.com>
2011-06-16 21:43:26 +0800

    update

--- makelink.sh
+++ makelink.sh
@@ -1,26 +1,45 @@
 #!/bin/sh
+# Plato Wu,2011/06/11: TODO use dirname of link file to decide if link is OK instead of
+# link file judgement
 
 linkfiles=".emacs .bash_profile .sbclrc .bash_logout .gnus.el
- .ssh .subversion/config .sawfishrc .screenrc .hgrc .hgignore 
-.gitconfig .gitignore .inputrc .bashrc .stumpwmrc .Xresources"
+.ssh .subversion/config .sawfishrc .screenrc .hgrc .hgignore 
+.gitconfig .gitignore .inputrc .bashrc .Xresources .stumwprc"
 #linkfiles="test test1 test2/test2"
 #echo $linkfiles
-filehome=~/linux-initial
+filehome=linux-initial
+
+linkfile(){
+    if [ $2 ]; then
+        mv $linkfile $linkfile.old
+        echo "make a copy with", $linkfile
+    fi
+
+    if [ "$1" == ".emacs" ]; then
+        ln -s $filehome/emacs/.emacs .emacs
+    else
+	ln -s $filehome/$linkfile $linkfile
+    fi
+
+    echo "make a link with", $linkfile
+}
+
 cd ~
+
 for linkfile in $linkfiles; do
     if ([ -d $linkfile ] || [ -f $linkfile ]) && [ ! -h $linkfile ]; then
-	mv $linkfile $linkfile.old
-	ln -s $filehome/$linkfile $linkfile
-	echo "make a backup and link with "$linkfile.old
+        linkfile $linkfile t
     elif [ ! -h $linkfile ]; then
-        if [ "$linkfile" == ".emacs" ]; then
-            ln -s $filehome/emacs/.emacs .emacs
-        else
-	    ln -s $filehome/$linkfile $linkfile
-        fi
-	echo "make a link with", $linkfile
+        linkfile $linkfile
     else
-        echo "do nothing with", $linkfile	
+        source=`readlink $linkfile`
+        # Plato Wu,2011/06/16: == =~ require [[]]
+        # Plato Wu,2011/06/16: or =~ linux-initial.*
+        if [[ ${source} == ${filehome}* ]]; then
+            echo "do nothing with", $linkfile	
+        else
+            linkfile $linkfile t
+        fi
     fi
 done
 
