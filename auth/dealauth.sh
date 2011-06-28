c59e26716a3221620c55fb866c0f98c957ac1c6e
Plato Wu <gtalk000@gmail.com>
2011-06-28 08:56:41 +0000

    update

--- auth/dealauth.sh
+++ auth/dealauth.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 filehome=~/linux-initial/auth
 authfiles=".authinfo .hgauth id_rsa"
 for authfile in $authfiles; do
@@ -12,9 +12,12 @@ for authfile in $authfiles; do
         mv $authfile $authfile.old
         echo "make a backup and link with "$authfile.old
     fi
+    # for send/receive public key
     # gpg --send-keys –keyserver subkeys.pgp.net gtalk000@gmail.com
     # gpg --keyserver subkeys.pgp.net --recv-keys gtalk000@gmail.com
+    # for export, copy  and import private key
     # gpg --export-secret-keys -a -o private.asc
+    # scp -P 80 netawater.blogdns.org://home/plato/backup/private.asc .
     # gpg --import private.asc
     #gpg -e -a -r "Plato Wu" $authfile will encrypt file with public key
     gpg -o $authfile -d $filehome/$authfile.asc
@@ -26,7 +29,5 @@ for authfile in $authfiles; do
             rm $authfile.old
         fi
     fi    
-    if [ "$authfile" == "id_rsa" ]; then
-        chmod 600 id_rsa
-    fi
+        chmod 600  $authfile
 done
