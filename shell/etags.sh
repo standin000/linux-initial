7d56af8d29f0762df1127968946d9caf105c1a7b
Plato Wu <gtalk000@gmail.com>
2011-06-30 23:09:36 +0800

    update

--- shell/etags.sh
+++ shell/etags.sh
@@ -2,4 +2,5 @@
 # Plato Wu,2009/11/18: iname let the match being case insensitive
 # -print0 and -0 let find and xargs handle file name which contain space.
 # Plato Wu,2010/01/03: * must be escaped for it is used by Shell first.
-find . -iname \*.[chs] -print0 | xargs -0 etags
+# Plato Wu,2011/06/28: archlinux provides etags.emacs, don't create link
+find . -iname \*.[chs] -print0 | xargs -0 etags.emacs
