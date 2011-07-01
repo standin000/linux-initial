cfc1e035fa5240baa695a46bc192c50513e9839a
Plato Wu <gtalk000@gmail.com>
2011-07-01 16:01:04 +0800

    update for cygwin

--- emacs/el-get-package.el
+++ emacs/el-get-package.el
@@ -21,34 +21,43 @@
      (expand-file-name "~/.emacs.d/elpa/package.el"))
   (package-initialize))
 
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "http://github.com/dimitri/el-get/raw/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
+
 ;; Plato Wu, 2010/12/17, this function should not use el-get-dir which is conflict with
 ;; el-get package itself
-(let ((el-get-install-dir        (expand-file-name "~/.emacs.d/el-get/"))
-      (package           "el-get"))
-  (unless (file-directory-p (concat el-get-install-dir package))
-    (let* ((bname             "*el-get bootstrap*")
-           (dummy             (unless (file-directory-p el-get-install-dir)
-                                (make-directory el-get-install-dir t)))
-           (pdir              (concat (file-name-as-directory el-get-install-dir) package))
-           (git               (or (executable-find "git") (error "Unable to find `git'")))
-; Plato Wu,2010/10/11: use sslVerify = false in git config skip https verification in http clone
-           (url               "https://github.com/dimitri/el-get.git")
-           (el-get-sources    `((:name ,package :type "git" :url ,url :features el-get :compile "el-get.el")))
-           (default-directory el-get-install-dir)
-           (process-connection-type nil) ; pipe, no pty (--no-progress)
-           (status            (call-process git nil bname t "--no-pager" "clone" "-v" url package)))
-      (set-window-buffer (selected-window) bname)
-      (when (eq 0 status)
-        (load (concat (file-name-as-directory pdir) package ".el"))
-        ;; (require 'bytecomp)
-        (el-get-init "el-get")
-        (with-current-buffer bname
-          (goto-char (point-max))
-          (insert "\nCongrats, el-get is installed and ready to serve!"))))))
+;; (let ((el-get-install-dir        (expand-file-name "~/.emacs.d/el-get/"))
+;;       (package           "el-get"))
+;;   (unless (file-directory-p (concat el-get-install-dir package))
+;;     (let* ((bname             "*el-get bootstrap*")
+;;            (dummy             (unless (file-directory-p el-get-install-dir)
+;;                                 (make-directory el-get-install-dir t)))
+;;            (pdir              (concat (file-name-as-directory el-get-install-dir) package))
+;;            (git               (or (executable-find "git") (error "Unable to find `git'")))
+;; ; Plato Wu,2010/10/11: use sslVerify = false in git config skip https verification in http clone
+;;            (url               "https://github.com/dimitri/el-get.git")
+;;            (el-get-sources    `((:name ,package :type "git" :url ,url :features el-get :compile "el-get.el")))
+;;            (default-directory el-get-install-dir)
+;;            (process-connection-type nil) ; pipe, no pty (--no-progress)
+;;            (status            (call-process git nil bname t "--no-pager" "clone" "-v" url package)))
+;;       (set-window-buffer (selected-window) bname)
+;;       (when (eq 0 status)
+;;         (load (concat (file-name-as-directory pdir) package ".el"))
+;;         ;; (require 'bytecomp)
+;;         (el-get-init "el-get")
+;;         (with-current-buffer bname
+;;           (goto-char (point-max))
+;;           (insert "\nCongrats, el-get is installed and ready to serve!"))))))
 
-(let ((default-directory "~/.emacs.d/"))
-  (setq load-path (cons default-directory load-path))
-  (normal-top-level-add-subdirs-to-load-path))
+;; (let ((default-directory "~/.emacs.d/"))
+;;   (setq load-path (cons default-directory load-path))
+;;   (normal-top-level-add-subdirs-to-load-path))
 
 (require 'el-get)
 
@@ -64,6 +73,8 @@
       ;; Plato Wu,2011/01/23: It report Package el-get failed to install, remove it first.
       ;; so remove el-get from el-get-sources
       '(;nxhtml
+        ;(:name dictionary-el    :type apt-get)
+        ;(:name emacs-goodies-el :type apt-get)
         vkill 
         ;; Plato Wu,2011/02/24: ido will add ido-configuration into after-load-alist
         ;; which cause error, so must use features.
@@ -85,28 +96,44 @@
         (:name sawfish :features sawfish :after sawfish-configuration)
         (:name google-c-style :features google-c-style
                :after (lambda () (add-hook 'c-mode-common-hook 'google-set-c-style)))
-        (:name session :features session :after session-configuration)
         (:name psvn :features psvn :after psvn-configuration)
         (:name dired-single :features dired-single :after dired-single-configuration)
-        (:name auctex :after auctex-configuration)
         (:name ascii :after ascii-configuration)
         ;; Plato Wu,2011/05/15: vi can be used in it.
         (:name multi-term :features multi-term)
         ;; Plato Wu,2011/05/23: quicklisp will install slime
 ;        (:name slime :after slime-configuration)
         (:name cldoc :features cldoc :after cldoc-configuration)
-        redshank
-        dpans2texi))
 
-(when (executable-find "w3m")
+	))
+
+
+
+(if (not (is-system "cygwin"))
+  (progn (setq el-get-sources
+         (append el-get-sources
+                 '((:name auctex :after auctex-configuration)
+                   (:name session :features session :after session-configuration)
+                   redshank dpans2texi)))
+         (when (executable-find "w3m") 
+           (setq el-get-sources
+                 (append el-get-sources
+                         '((:name emacs-w3m :features w3m
+                                  :after w3m-configuration)
+                           (:name weblogger :type elpa :features weblogger :after blogger-configuration) 
+                           (:name org-toodledo :features org-toodledo :after org-toodledo-configuration))))))
   (setq el-get-sources
         (append el-get-sources
-                '((:name emacs-w3m :features w3m
-                         :after w3m-configuration) 
-                  (:name weblogger :type elpa :features weblogger :after blogger-configuration)
-                  (:name org-toodledo :features org-toodledo :after org-toodledo-configuration)))))
-        
-(el-get 'sync)
+                '((:name org-toodledo :features org-toodledo :after org-toodledo-configuration)))))
 
-;:features auctex
+        
+;(el-get 'sync)
+
+(setq my-packages
+      (append
+       '(el-get)
+       (mapcar 'el-get-source-name el-get-sources)))
+
+(el-get 'sync my-packages)
+
 (provide 'el-get-package)
--- makelink.sh
+++ makelink.sh
@@ -4,7 +4,7 @@
 
 linkfiles=".emacs .bash_profile .sbclrc .bash_logout .gnus.el
 .ssh .subversion/config .sawfishrc .screenrc .hgrc .hgignore 
-.gitconfig .gitignore .inputrc .bashrc .Xresources .stumwprc"
+.gitconfig .gitignore .inputrc .bashrc .Xresources .stumpwmrc"
 #linkfiles="test test1 test2/test2"
 #echo $linkfiles
 filehome=~/linux-initial
