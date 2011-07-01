791081bdef37526df5b7582a04ddb108aa6e6731
Plato Wu <gtalk000@gmail.com>
2011-07-01 16:55:48 +0800

    update

--- emacs/el-get-package.el
+++ emacs/el-get-package.el
@@ -23,7 +23,73 @@
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-(unless (require 'el-get nil t)
+(if (require 'el-get nil t)
+    (progn
+      (setq el-get-recipe-path 
+            (cons "~/linux-initial/emacs/recipes/" el-get-recipe-path))
+
+      ;; Plato Wu,2011/05/07: remove it for require will search it first for package,
+      ;; and the recipe file name is the same as package file name
+      (setq load-path 
+            (remove (expand-file-name "~/.emacs.d/el-get/el-get/recipes") load-path))
+      ;; Plato Wu,2010/12/26: features don't works for elpa type
+      (setq el-get-sources
+            ;; Plato Wu,2011/01/23: It report Package el-get failed to install, remove it first.
+            ;; so remove el-get from el-get-sources
+            '(;nxhtml
+                                        ;(:name dictionary-el    :type apt-get)
+                                        ;(:name emacs-goodies-el :type apt-get)
+              vkill 
+              ;; Plato Wu,2011/02/24: ido will add ido-configuration into after-load-alist
+              ;; which cause error, so must use features.
+              (:name ido-hacks :features ido-hacks :after ido-configuration) 
+              (:name magit :features magit
+                     :after (lambda () (global-set-key (kbd "C-x C-z") 'magit-status)))
+              (:name paredit :features paredit :after paredit-configuration)
+              (:name emms :type elpa :features emms :after emms-configuration)
+              (:name htmlize :type elpa :features htmlize)
+              ;; Plato Wu,2011/01/03: when I start emacs as a daemon, it require ImageMagick
+              ;; get installed to pass error.
+              (:name muse :type elpa :features muse :after muse-configuration)
+                                        ;        (:name xml-rpc :type elpa)
+              ;; Plato Wu,2011/01/30: both lisppaste and weblogger require xml-rpc, el-get can't
+              ;; deal with correctly, it report xml-rpc existed when try to install weblogger after
+              ;; lisppaste, so disable lisppaste first, it is not useful for me.
+                                        ;	(:name lisppaste :type elpa)        
+              (:name smart-tab :features smart-tab :after smart-tab-configuration)
+              (:name sawfish :features sawfish :after sawfish-configuration)
+              (:name google-c-style :features google-c-style
+                     :after (lambda () (add-hook 'c-mode-common-hook 'google-set-c-style)))
+              (:name psvn :features psvn :after psvn-configuration)
+              (:name dired-single :features dired-single :after dired-single-configuration)
+              (:name ascii :after ascii-configuration)
+              ;; Plato Wu,2011/05/15: vi can be used in it.
+              (:name multi-term :features multi-term)
+              ;; Plato Wu,2011/05/23: quicklisp will install slime
+                                        ;        (:name slime :after slime-configuration)
+              (:name cldoc :features cldoc :after cldoc-configuration)))
+
+      (if (not (is-system "cygwin"))
+          (progn (setq el-get-sources
+                       (append el-get-sources
+                               '((:name auctex :after auctex-configuration)
+                                 (:name session :features session :after session-configuration)
+                                 redshank dpans2texi)))
+                 (when (executable-find "w3m") 
+                   (setq el-get-sources
+                         (append el-get-sources
+                                 '((:name emacs-w3m :features w3m
+                                          :after w3m-configuration)
+                                   (:name weblogger :type elpa :features weblogger :after blogger-configuration) 
+                                   (:name org-toodledo :features org-toodledo :after org-toodledo-configuration))))))
+        (setq el-get-sources
+              (append el-get-sources
+                      '((:name org-toodledo :features org-toodledo :after org-toodledo-configuration)))))
+      (setq my-packages
+            (append
+             '(el-get)
+             (mapcar 'el-get-source-name el-get-sources)))
+      (el-get 'wait my-packages))
   (url-retrieve
    "http://github.com/dimitri/el-get/raw/master/el-get-install.el"
    (lambda (s)
@@ -58,82 +124,5 @@
 ;; (let ((default-directory "~/.emacs.d/"))
 ;;   (setq load-path (cons default-directory load-path))
 ;;   (normal-top-level-add-subdirs-to-load-path))
-
-(require 'el-get)
-
-(setq el-get-recipe-path 
-      (cons "~/linux-initial/emacs/recipes/" el-get-recipe-path))
-
-;; Plato Wu,2011/05/07: remove it for require will search it first for package,
-;; and the recipe file name is the same as package file name
-(setq load-path 
-      (remove (expand-file-name "~/.emacs.d/el-get/el-get/recipes") load-path))
-;; Plato Wu,2010/12/26: features don't works for elpa type
-(setq el-get-sources
-      ;; Plato Wu,2011/01/23: It report Package el-get failed to install, remove it first.
-      ;; so remove el-get from el-get-sources
-      '(;nxhtml
-        ;(:name dictionary-el    :type apt-get)
-        ;(:name emacs-goodies-el :type apt-get)
-        vkill 
-        ;; Plato Wu,2011/02/24: ido will add ido-configuration into after-load-alist
-        ;; which cause error, so must use features.
-        (:name ido-hacks :features ido-hacks :after ido-configuration) 
-        (:name magit :features magit
-               :after (lambda () (global-set-key (kbd "C-x C-z") 'magit-status)))
-        (:name paredit :features paredit :after paredit-configuration)
-        (:name emms :type elpa :features emms :after emms-configuration)
-        (:name htmlize :type elpa :features htmlize)
-        ;; Plato Wu,2011/01/03: when I start emacs as a daemon, it require ImageMagick
-        ;; get installed to pass error.
-        (:name muse :type elpa :features muse :after muse-configuration)
-;        (:name xml-rpc :type elpa)
-        ;; Plato Wu,2011/01/30: both lisppaste and weblogger require xml-rpc, el-get can't
-        ;; deal with correctly, it report xml-rpc existed when try to install weblogger after
-        ;; lisppaste, so disable lisppaste first, it is not useful for me.
-;	(:name lisppaste :type elpa)        
-        (:name smart-tab :features smart-tab :after smart-tab-configuration)
-        (:name sawfish :features sawfish :after sawfish-configuration)
-        (:name google-c-style :features google-c-style
-               :after (lambda () (add-hook 'c-mode-common-hook 'google-set-c-style)))
-        (:name psvn :features psvn :after psvn-configuration)
-        (:name dired-single :features dired-single :after dired-single-configuration)
-        (:name ascii :after ascii-configuration)
-        ;; Plato Wu,2011/05/15: vi can be used in it.
-        (:name multi-term :features multi-term)
-        ;; Plato Wu,2011/05/23: quicklisp will install slime
-;        (:name slime :after slime-configuration)
-        (:name cldoc :features cldoc :after cldoc-configuration)
-
-	))
-
-
-
-(if (not (is-system "cygwin"))
-  (progn (setq el-get-sources
-         (append el-get-sources
-                 '((:name auctex :after auctex-configuration)
-                   (:name session :features session :after session-configuration)
-                   redshank dpans2texi)))
-         (when (executable-find "w3m") 
-           (setq el-get-sources
-                 (append el-get-sources
-                         '((:name emacs-w3m :features w3m
-                                  :after w3m-configuration)
-                           (:name weblogger :type elpa :features weblogger :after blogger-configuration) 
-                           (:name org-toodledo :features org-toodledo :after org-toodledo-configuration))))))
-  (setq el-get-sources
-        (append el-get-sources
-                '((:name org-toodledo :features org-toodledo :after org-toodledo-configuration)))))
-
-        
-;(el-get 'sync)
-
-(setq my-packages
-      (append
-       '(el-get)
-       (mapcar 'el-get-source-name el-get-sources)))
-
-(el-get 'sync my-packages)
 
 (provide 'el-get-package)
