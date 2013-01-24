a307ed4d752b4dd4846033575aac4fd97aa9fc15
Plato Wu <gtalk000@gmail.com>
2013-01-24 16:35:22 +0800

    update dealauth.sh

--- auth/dealauth.sh
+++ auth/dealauth.sh
@@ -13,8 +13,8 @@ for authfile in $authfiles; do
         echo "make a backup and link with "$authfile.old
     fi
     # for send/receive public key
-    # gpg --send-keys –keyserver subkeys.pgp.net gtalk000@gmail.com
-    # gpg --keyserver subkeys.pgp.net --recv-keys gtalk000@gmail.com
+    # gpg --send-keys --keyserver subkeys.pgp.net gtalk000@gmail.com
+    # gpg --keyserver subkeys.pgp.net --recv-keys E87C1128
     # for export, copy  and import private key
     # gpg --export-secret-keys -a -o private.asc
     # scp -P 80 plato@netawater.blogdns.org://home/plato/backup/private.asc .
