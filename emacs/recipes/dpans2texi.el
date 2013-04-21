1bb943230612389abcedce801e863ad476ec2d8c
Plato Wu <gtalk000@gmail.com>
2013-04-21 09:25:47 +0800

    update

--- emacs/recipes/dpans2texi.el
+++
@@ -1,6 +0,0 @@
-(:name :dpans2texi
-      :type http-tar
-      :options ("xzf")
-      :module "dpans2texi"
-      :url "http://users-phys.au.dk/harder/dpans2texi-1.05.tar.gz"
-      :build ("./configure" "make wget" "make" "sudo make install"))
