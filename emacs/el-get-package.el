5dd4b1e3a0134ef101845abcf4055dce578e9035
Plato Wu <gtalk000@gmail.com>
2014-04-27 17:18:06 +0800

    update for nabla

--- emacs/el-get-package.el
+++ emacs/el-get-package.el
@@ -1,25 +1,26 @@
 ;; Plato Wu,2014/04/20: el-get need git, make, mercurial, subversion, cvs
 ;; Plato Wu,2013/06/03: emacs 24 support ELPA natively
-(if (is-version 24)
-  (package-initialize)
-  (unless (file-directory-p (expand-file-name "~/.emacs.d/elpa/"))
-    (let ((buffer (url-retrieve-synchronously
-               "http://tromey.com/elpa/package-install.el")))
-      (save-excursion
-        (set-buffer buffer)
-        (goto-char (point-min))
-        (re-search-forward "^$" nil 'move)
-        (eval-region (point) (point-max))
-        (kill-buffer (current-buffer)))))
-  (load (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
+;; (if (is-version 24)
+;;   (package-initialize)
+;;   (unless (file-directory-p (expand-file-name "~/.emacs.d/elpa/"))
+;;     (let ((buffer (url-retrieve-synchronously
+;;                "http://tromey.com/elpa/package-install.el")))
+;;       (save-excursion
+;;         (set-buffer buffer)
+;;         (goto-char (point-min))
+;;         (re-search-forward "^$" nil 'move)
+;;         (eval-region (point) (point-max))
+;;         (kill-buffer (current-buffer)))))
+;;   (load (expand-file-name "~/.emacs.d/elpa/package.el"))
+;;   (package-initialize))
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+       ;; Plato Wu,2014/03/18: nintfloor don't support TLS
+       "http://raw.github.com/dimitri/el-get/master/el-get-install.el")
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
@@ -35,11 +36,16 @@
 ;; use eval-after-load to call :after function, it is annoy style.
 
 ;; Set el-get-sources and call el-get to init all those packages we need.
+(unless (is-version 24)
+  ;; Plato Wu,2013/06/13: emacs below 24.3 need it
+  (setq el-get-sources
+        '((:name cl-lib :type elpa))))
+
+
 (setq el-get-sources
-      '(
-        ;; Plato Wu,2013/06/13: emacs below 24.3 need it
-;        (:name cl-lib :type elpa)
-        (:name magit :after (global-set-key (kbd "C-x C-z") 'magit-status))
+      (append 
+       el-get-sources
+      '((:name magit :after (global-set-key (kbd "C-x C-z") 'magit-status))
         (:name org-mode :after 
                (eval-after-load "org" '(org-configuration)))
         (:name paredit :after (paredit-configuration))
@@ -60,7 +66,7 @@
       ;; deal with correctly, it report xml-rpc existed when try to install weblogger after
       ;; lisppaste, so disable lisppaste first, it is not useful for me.
       ;; (:name lisppaste :type elpa)        
-        ))
+        )))
 
 (when (not (is-system "cygwin"))
   (setq el-get-sources
@@ -73,15 +79,15 @@
                                (turn-on-xclip)))) 
                   (:name clojure-mode :type elpa)
       ;           (:name clojure-test-mode :type elpa)
-                  (:name nrepl) 
+                  ;; Plato Wu,2014/03/18: no this package in the latest el-get
+;                  (:name nrepl) 
       ;           (:name auctex :after auctex-configuration)
                   (:name htmlize :type elpa)
                   (:name muse :type elpa)
                   (:name sawfish :after (sawfish-configuration))
+                  ;; Plato Wu,2014/03/18: it is very old, need a good replacement
                  ;(:name dired-single :features dired-single :after (dired-single-configuration))
-                  (:name dired-single)
                   (:name multi-term)
-                  (:name emms :type elpa :after (emms-configuration))
                   ;; Plato Wu,2011/07/02: it seems there is a problem with session
                   ;; recipe in el-get
       ;            (:name session :features session :after session-configuration)
@@ -95,11 +101,14 @@
           (append el-get-sources
                   '((:name emacs-w3m :after (w3m-configuration))
                     (:name weblogger :type elpa :after 
-                           (eval-after-load "muse-mode" '(blogger-configuration)))
-                    )))))
+                           (eval-after-load "muse-mode" '(blogger-configuration)))))))
+  (when (executable-find "mpd")
+    (setq el-get-sources
+          (append el-get-sources
+                  (:name emms :type elpa :after (emms-configuration))))))
 (setq my-packages
       (append
-       '(el-get)
+       '("el-get")
        (mapcar 'el-get-source-name el-get-sources)))
 
 (el-get 'sync my-packages)
